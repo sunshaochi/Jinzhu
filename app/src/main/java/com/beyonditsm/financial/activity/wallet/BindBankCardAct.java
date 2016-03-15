9f32619e6218c54fd621f4f1720b286eed113478
王梦阳 <279809190@qq.com>
2016-03-15 12:41:46 +0800

    绑定银行卡

---
+++ app/src/main/java/com/beyonditsm/financial/activity/wallet/BindBankCardAct.java
@@ -0,0 +1,21 @@
+package com.beyonditsm.financial.activity.wallet;
+
+import android.os.Bundle;
+
+import com.beyonditsm.financial.R;
+import com.beyonditsm.financial.activity.BaseActivity;
+
+/**
+ * Created by Administrator on 2016/3/15.
+ */
+public class BindBankCardAct extends BaseActivity {
+    @Override
+    public void setLayout() {
+        setContentView(R.layout.act_bindbankcard);
+    }
+
+    @Override
+    public void init(Bundle savedInstanceState) {
+
+    }
+}
