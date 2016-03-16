b8cc7f62ac00ca94429cb2b437958b0d2fe541b2
王斌 <493204277@qq.com>
2016-03-16 19:42:49 +0800

    bug修改

--- app/src/main/java/com/beyonditsm/financial/util/GeneralUtils.java
+++ app/src/main/java/com/beyonditsm/financial/util/GeneralUtils.java
@@ -32,24 +32,22 @@ import org.json.JSONException;
 import java.io.File;
 
 /**
- *
  * Created by wangbin on 15/11/23.
  */
 public class GeneralUtils {
-
 
 
     public void getCode(String phoneNum) {
         RequestManager.getCommManager().getCode(phoneNum, new RequestManager.CallBack() {
             @Override
             public void onSucess(String result) {
-                MyToastUtils.showShortToast(MyApplication.getInstance(),"验证码发送成功");
+                MyToastUtils.showShortToast(MyApplication.getInstance(), "验证码发送成功");
                 iCode.isSucess(1);
             }
 
             @Override
-            public void onError(int status,String msg) {
-                MyToastUtils.showShortToast(MyApplication.getInstance(),msg);
+            public void onError(int status, String msg) {
+                MyToastUtils.showShortToast(MyApplication.getInstance(), msg);
                 iCode.isSucess(-1);
             }
         });
@@ -66,21 +64,22 @@ public class GeneralUtils {
     }
 
 
-    public void toVersion(final Context context, final int currentVersion,final int type){
-        MyLogUtils.info("当前版本："+currentVersion+"");
+    public void toVersion(final Context context, final int currentVersion, final int type) {
+        MyLogUtils.info("当前版本：" + currentVersion + "");
         RequestManager.getCommManager().toVersion(currentVersion, "ANDROID", new RequestManager.CallBack() {
             @Override
             public void onSucess(String result) throws JSONException {
-                ResultData<VersionInfo> rd= (ResultData<VersionInfo>) GsonUtils.json(result,VersionInfo.class);
+                ResultData<VersionInfo> rd = (ResultData<VersionInfo>) GsonUtils.json(result, VersionInfo.class);
                 if (!rd.getData().isNeedUpdrage()) {
-                    if(type==0)
-                    MyToastUtils.showShortToast(context, "当前已是最新版本！");
+                    if (type == 0)
+                        MyToastUtils.showShortToast(context, "当前已是最新版本！");
 
-                }else{
+                } else {
                     String path = rd.getData().getVersion().getPackagePath();
                     String updrageLog = rd.getData().getVersion().getUpdrageLog();//新版本更新内容
                     String remark = rd.getData().getVersion().getRemark();//新版本大小
-                    showIsDownLoad(context, path,"",remark,updrageLog);
+                    String versionName = rd.getData().getVersion().getVersionName();
+                    showIsDownLoad(context, path, versionName, remark, updrageLog);
 //                MyToastUtils.showShortToast(MyApplication.getInstance(),rd.getData().getMessage());
                 }
             }
@@ -96,34 +95,35 @@ public class GeneralUtils {
     /*
  * 从服务器中下载APK
  */
-    protected void downLoadApk(final Context context,final String url) {
+    protected void downLoadApk(final Context context, final String url) {
         final ProgressDialog pd;    //进度条对话框
-        pd = new  ProgressDialog(context);
+        pd = new ProgressDialog(context);
         pd.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
         pd.setMax(100);
         pd.setProgress(0);
         pd.setMessage("正在下载更新");
         pd.show();
-        new Thread(){
+        new Thread() {
             @Override
             public void run() {
                 try {
                     File file = DownLoadManager.getFileFromServer(url, pd);
                     sleep(500);
-                    installApk(context,file);
+                    installApk(context, file);
                     pd.dismiss(); //结束掉进度条对话框
                 } catch (Exception e) {
                     Message msg = new Message();
                     msg.what = DOWN_ERROR;
-                    msg.obj=context;
+                    msg.obj = context;
                     handler.sendMessage(msg);
                     e.printStackTrace();
                 }
-            }}.start();
+            }
+        }.start();
     }
 
-    private final int DOWN_ERROR=1;
-    Handler handler = new Handler(){
+    private final int DOWN_ERROR = 1;
+    Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             // TODO Auto-generated method stub
@@ -131,16 +131,16 @@ public class GeneralUtils {
             switch (msg.what) {
 
                 case DOWN_ERROR:
-                    Context context= (Context) msg.obj;
+                    Context context = (Context) msg.obj;
                     //下载apk失败
-                    Toast.makeText(context, "下载新版本失败",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(context, "下载新版本失败", Toast.LENGTH_SHORT).show();
                     break;
             }
         }
     };
 
     //安装apk
-    protected void installApk(Context context,File file) {
+    protected void installApk(Context context, File file) {
         Intent intent = new Intent();
         //执行动作
         intent.setAction(Intent.ACTION_VIEW);
@@ -148,7 +148,6 @@ public class GeneralUtils {
         intent.setDataAndType(Uri.fromFile(file), "application/vnd.android.package-archive");//编者按：此处Android应为android，否则造成安装不了
         context.startActivity(intent);
     }
-
 
 
     private NotificationManager mNotificationManager = null;
@@ -160,37 +159,37 @@ public class GeneralUtils {
     /**
      * 是否下载
      */
-    private void showIsDownLoad(final Context context,final String path,String versionName,String versionSize,String versionContent) {
+    private void showIsDownLoad(final Context context, final String path, String versionName, String versionSize, String versionContent) {
         MyAlertDialog dialog = new MyAlertDialog(context).builder();
-        dialog.setTitle("提示").setMsg("检测到最新版本，是否现在下载？").setCancelable(false)
-                .setPositiveButton("立即更新", new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
 //        dialog.setTitle("提示").setMsg("检测到最新版本，是否现在下载？").setCancelable(false)
-//                .setPositiveButton("确定", new View.OnClickListener() {
+//                .setPositiveButton("立即更新", new View.OnClickListener() {
 //                    @Override
 //                    public void onClick(View v) {
-//                        File file = new File(fileName);
-//                        if (file.exists()) {
-//                            file.delete();
-//                        }
-//                        downLoad(context,path);
-                        downLoadApk(context, path);
-                    }
-                }).setNegativeButton("稍后再说", null).show();
-//                    }
-//                }).setNegativeButton("取消", null).show();
-//        dialog.setTitle("发现新版本").setMsgLayout(R.layout.layout_versionupload,"",versionSize,versionContent).setCancelable(false).setPositiveButton("立即更新", new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-////                File file = new File(fileName);
+////        dialog.setTitle("提示").setMsg("检测到最新版本，是否现在下载？").setCancelable(false)
+////                .setPositiveButton("确定", new View.OnClickListener() {
+////                    @Override
+////                    public void onClick(View v) {
+////                        File file = new File(fileName);
 ////                        if (file.exists()) {
 ////                            file.delete();
 ////                        }
 ////                        downLoad(context,path);
-//                downLoadApk(context,path);
-//            }
-//        }).setNegativeButton("稍后再说", null).show();
+//                        downLoadApk(context, path);
+//                    }
+//                }).setNegativeButton("稍后再说", null).show();
+//                    }
+//                }).setNegativeButton("取消", null).show();
+        dialog.setTitle("发现新版本").setMsgLayout(R.layout.layout_versionupload, versionName, versionSize, versionContent).setCancelable(false).setPositiveButton("立即更新", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                File file = new File(fileName);
+//                        if (file.exists()) {
+//                            file.delete();
+//                        }
+//                        downLoad(context,path);
+                downLoadApk(context, path);
+            }
+        }).setNegativeButton("稍后再说", null).show();
     }
 
     /**
@@ -198,17 +197,18 @@ public class GeneralUtils {
      *
      * @param path
      */
-    @SuppressWarnings({ "unused", "rawtypes" })
-    private void downLoad(final Context context,String path) {
+    @SuppressWarnings({"unused", "rawtypes"})
+    private void downLoad(final Context context, String path) {
         HttpUtils http = new HttpUtils();
         HttpHandler handler = http.download(path, fileName, true, true,
                 new RequestCallBack<File>() {
                     Intent intent = new Intent(SettingAct.ISLOADING);
+
                     @Override
                     public void onLoading(long total, long current,
                                           boolean isUploading) {
                         isUploading = true;
-                        intent.putExtra("isUploading",isUploading);
+                        intent.putExtra("isUploading", isUploading);
                         context.sendBroadcast(intent);
                         fileSize = (int) total;
                         downLoadFileSize = (int) current;
@@ -218,12 +218,12 @@ public class GeneralUtils {
 
                     @Override
                     public void onStart() {
-                        isStart =true;
-                        intent.putExtra("isStart",isStart);
+                        isStart = true;
+                        intent.putExtra("isStart", isStart);
                         context.sendBroadcast(intent);
-                        Message message=myhandler.obtainMessage();
-                        message.obj=context;
-                        message.what=0;
+                        Message message = myhandler.obtainMessage();
+                        message.obj = context;
+                        message.what = 0;
                         myhandler.sendMessage(message);
                         MyToastUtils.showShortToast(context, "开始升级新版本");
                     }
@@ -264,7 +264,7 @@ public class GeneralUtils {
             super.handleMessage(msg);
             switch (msg.what) {
                 case 0:
-                    Context context=(Context) msg.obj;
+                    Context context = (Context) msg.obj;
                     showNotification(context);
                     break;
                 case 1:
