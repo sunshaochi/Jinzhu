54f3b74fd9d279d59d8650bed4a777b7ff8cdb9d
王斌 <493204277@qq.com>
2016-03-21 09:58:05 +0800

    隐藏绑定银行卡

--- app/src/main/java/com/beyonditsm/financial/http/IFinancialUrl.java
+++ app/src/main/java/com/beyonditsm/financial/http/IFinancialUrl.java
@@ -10,19 +10,19 @@ public interface IFinancialUrl {
     /*test1地址*/
 //    String BASE_URL = "http://139.196.111.82:5018";
     /*正式地址*/
-//    String BASE_URL = "http://option.myjinzhu.com";
+    String BASE_URL = "http://option.myjinzhu.com";
 
     /*测试地址2*/
-    String BASE_URL = "http://test.myjinzhu.com:8082";
+//    String BASE_URL = "http://test.myjinzhu.com:8082";
 //    String BASE_URL = "http://option.myjinzhu.com";
 
     /*正式游戏地址*/
-//    String GAME_URL = "http://m.farm.myjinzhu.com/";
+    String GAME_URL = "http://m.farm.myjinzhu.com/";
 
     /*测试游戏地址1*/
 //    String GAME_URL = "http://myjinzhu.com:5011/";
     /*测试游戏地址2*/
-    String GAME_URL = "http://test.myjinzhu.com:63";
+//    String GAME_URL = "http://test.myjinzhu.com:63";
     String API_VERSION = "/easyplay";
 
     String BASE_IMAGE_URL = BASE_URL + API_VERSION + "/";
