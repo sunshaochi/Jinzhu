24f889e1bfacc1a83da65fa59ce5e505cd080aa7
李文康 <liwk_liwk@sina.com>
2016-07-18 11:18:00 +0800

    导入MyApplication包

--- app/src/main/java/com/beyonditsm/financial/fragment/HomeFragment.java
+++ app/src/main/java/com/beyonditsm/financial/fragment/HomeFragment.java
@@ -14,6 +14,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.beyonditsm.financial.ConstantValue;
+import com.beyonditsm.financial.MyApplication;
 import com.beyonditsm.financial.R;
 import com.beyonditsm.financial.activity.credit.CreditGuideAct;
 import com.beyonditsm.financial.activity.user.GameActivity;
