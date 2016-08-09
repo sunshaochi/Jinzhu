ed581c50aa3b24f42fc46f08a20349adcb4f0f56
xly <zhaoyh294@163.com>
2016-08-09 17:16:16 +0800

    ff

---
+++ app/src/main/java/com/beyonditsm/financial/activity/PermissionsActivity.java
@@ -0,0 +1,13 @@
+package com.beyonditsm.financial.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PermissionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_permissions);
+    }
+}
---
+++ app/src/main/java/com/beyonditsm/financial/util/PermissionsChecker.java
@@ -0,0 +1,7 @@
+package com.beyonditsm.financial.util;
+
+/**
+ * Created by xuleyuan on 2016/8/5.
+ */
+public class PermissionsChecker {
+}
