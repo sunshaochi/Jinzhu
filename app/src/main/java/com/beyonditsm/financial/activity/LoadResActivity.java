eec0dd48a56205a340c71220f26749c75b1e5cf7
xly <zhaoyh294@163.com>
2016-08-18 13:24:40 +0800

    sdsds

---
+++ app/src/main/java/com/beyonditsm/financial/activity/LoadResActivity.java
@@ -0,0 +1,51 @@
+package com.beyonditsm.financial.activity;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.support.multidex.MultiDex;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.beyonditsm.financial.MyApplication;
+import com.beyonditsm.financial.R;
+import com.lidroid.xutils.util.LogUtils;
+
+import scala.App;
+
+public class LoadResActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super .onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN , WindowManager.LayoutParams.FLAG_FULLSCREEN );
+        overridePendingTransition(R.anim.null_anim, R.anim.null_anim);
+        setContentView(R.layout.activity_load_res);
+        new LoadDexTask().execute();
+    }
+    class LoadDexTask extends AsyncTask {
+        @Override
+        protected Object doInBackground(Object[] params) {
+            try {
+                MultiDex.install(getApplication());
+                Log.d("loadDex" , "install finish" );
+                //// TODO: 2016/8/18 someThingMyHappen
+                MyApplication.getInstance().installFinish(getApplication());
+            } catch (Exception e) {
+                Log.e("loadDex" , e.getLocalizedMessage());
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Object o) {
+            Log.d( "loadDex", "get install finish");
+            finish();
+            System.exit( 0);
+        }
+    }
+    @Override
+    public void onBackPressed() {
+        //cannot backpress
+    }
