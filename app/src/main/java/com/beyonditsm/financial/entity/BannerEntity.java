288e66bc57edec953f6f9e3bfc45674e1ac5726a
xly <zhaoyh294@163.com>
2016-10-10 16:34:59 +0800

    踢脚3

---
+++ app/src/main/java/com/beyonditsm/financial/entity/BannerEntity.java
@@ -0,0 +1,42 @@
+package com.beyonditsm.financial.entity;
+
+/**
+ * banner
+ * Created by liwk on 2016/9/20 0020.
+ */
+public class BannerEntity {
+
+    /**
+     * bannerName : 中秋送红包
+     * imgSrc : http://test.myjinzhu.com:61/img/theme_banner/midAutumn_banner.png
+     * hrefAddr : http://activity.myjinzhu.com/activity/redPacketRain/index2/H5-1
+     */
+
+    private String bannerName;//banner名称
+    private String imgSrc;//图片链接
+    private String hrefAddr;//点击图片跳转链接
+
+    public String getBannerName() {
+        return bannerName;
+    }
+
+    public void setBannerName(String bannerName) {
+        this.bannerName = bannerName;
+    }
+
+    public String getImgSrc() {
+        return imgSrc;
+    }
+
+    public void setImgSrc(String imgSrc) {
+        this.imgSrc = imgSrc;
+    }
+
+    public String getHrefAddr() {
+        return hrefAddr;
+    }
+
+    public void setHrefAddr(String hrefAddr) {
+        this.hrefAddr = hrefAddr;
+    }
+}
