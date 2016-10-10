99effe7c4e0505354d594b58e0c1ef2a2864a9a8
xly <zhaoyh294@163.com>
2016-10-10 16:36:24 +0800

    踢脚4

---
+++ app/src/main/java/com/beyonditsm/financial/view/MyScrollView.java
@@ -0,0 +1,39 @@
+package com.beyonditsm.financial.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ScrollView;
+
+/**
+ * Created by Administrator on 2016/9/18 0018.
+ */
+public class MyScrollView extends ScrollView {
+    private OnScrollListener onSrollListener;
+    public MyScrollView(Context context) {
+        super(context);
+    }
+    public MyScrollView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+    public MyScrollView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public interface OnScrollListener{
+        void onScroll(MyScrollView myScrollView, int x, int y, int oldx, int oldy);
+    }
+    public void setOnScrollListener(OnScrollListener onScrollListener){
+        this.onSrollListener = onScrollListener;
+    }
+    public OnScrollListener getOnSrollListener(){
+        return onSrollListener;
+    }
+
+    @Override
+    protected void onScrollChanged(int l, int t, int oldl, int oldt) {
+        super.onScrollChanged(l, t, oldl, oldt);
+        if (onSrollListener!=null){
+            onSrollListener.onScroll(this,l,t,oldl,oldt);
+        }
+    }
+}
