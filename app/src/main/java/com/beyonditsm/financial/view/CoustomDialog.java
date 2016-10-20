ba532b92c934c07028db1351427f6e762e7c6db9
liwenkang <liwk_liwk@sina.com>
2016-10-20 15:12:37 +0800

    自定义dialog

--- app/src/main/java/com/beyonditsm/financial/view/CoustomDialog.java
+++ app/src/main/java/com/beyonditsm/financial/view/CoustomDialog.java
@@ -1,7 +1,13 @@
 package com.beyonditsm.financial.view;
 
 import android.content.Context;
+import android.view.Display;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.beyonditsm.financial.R;
 
@@ -13,11 +19,31 @@ import com.beyonditsm.financial.R;
 public class CoustomDialog {
 
     private Context context;
+    private Display display;
     public CoustomDialog(Context context) {
         this.context = context;
+        WindowManager windowManager = (WindowManager) context
+                .getSystemService(Context.WINDOW_SERVICE);
+        display = windowManager.getDefaultDisplay();
     }
     public CoustomDialog builder(){
-        LayoutInflater.from(context).inflate(R.layout.coustom_layout,null);
+        View view = LayoutInflater.from(context).inflate(R.layout.coustom_layout, null);
+
+
+        view.setMinimumWidth(498);
+        view.setMinimumHeight(300);
+        ImageView ivClose = (ImageView) view.findViewById(R.id.iv_close);
+        TextView tvLook = (TextView) view.findViewById(R.id.tv_look);
+        TextView tvCancel = (TextView) view.findViewById(R.id.tv_cancel);
+        ListView lvUnnatural = (ListView) view.findViewById(R.id.lv_unnatural);
+
+        ivClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        
         return this;
     }
 }
